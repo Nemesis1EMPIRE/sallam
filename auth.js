85150be49ddbe58fb9ca30525a8fb25faabe3faa
Nemesis1EMPIRE <137032212+Nemesis1EMPIRE@users.noreply.github.com>
2025-04-25 13:48:14 +0100

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,8 @@
+// auth.js
+document.addEventListener('DOMContentLoaded', function() {
+    const SUPABASE_URL = "https://ufpmrteapbfukhftrpcv.supabase.co";
+    const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVmcG1ydGVhcGJmdWtoZnRycGN2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU1Njc2OTcsImV4cCI6MjA2MTE0MzY5N30.GdoHerSP1ij8_m9L9562n5FXvHR7V6J-dxHc-70oPUs";
+    const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+    // ... [le reste de votre code JavaScript]
+});
